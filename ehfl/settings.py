7f36785ead87d84734c627a27747a9604dba5243
Jason Mow <jason.mow@gmail.com>
2013-06-08 18:52:53 -0700

    switching to postgres

--- ehfl/settings.py
+++ ehfl/settings.py
@@ -16,7 +16,7 @@ PARENT_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), '')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': PARENT_DIR + 'ehfl.sqlite',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
